dc3579518c207b2575d48a0bf0e834ae419a65c3
Jose Antonio <antoniojaraujo0@gmail.com>
2022-11-29 10:23:10 -0300

    card inicio

--- src/components/Destaques/index.jsx
+++ src/components/Destaques/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import tshirt from "../assets/t-shit.png";
 
-export default function Destaques (){
-  return (
-    <div>
-        Antonio/ Davi
-    </div>
-  )
+import styles from "./styles.module.scss";
+
+export default function Destaques() {
+	return (
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<div className={styles.leftSide}>
+					<span className={styles.cardDiscount}>
+						<p>30% OFF</p>
+					</span>
+					<h3 className={styles.cardTitle}>Novo drop Supreme</h3>
+					<button>Comprar</button>
+				</div>
+				<div className="rightSide">
+					<img src={tshirt} alt="camiseta" />
+				</div>
+			</div>
+		</div>
+	);
 }
