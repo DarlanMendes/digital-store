1430ee986dd73dbb4f9d48b06280b1787ae72feb
Jose Antonio <antoniojaraujo0@gmail.com>
2022-11-30 13:57:24 -0300

    card_destaques_coleção

--- src/components/CardDestaques/index.jsx
+++ src/components/CardDestaques/index.jsx
@@ -4,12 +4,50 @@ import styles from "./styles.module.scss";
 
 import tshirt from "../assets/t-shit.png";
 
+import calça from "../assets/calça.svg";
+import fone from "../assets/fone.svg";
+import camiseta from "../assets/camiseta.svg";
+import tenis from "../assets/Tenis.svg";
+
 export default function CardDestaques() {
 	return (
-		<div className={styles.cards}>
-			<Destaques productImage={`url(${tshirt} )`} />
-			<Destaques productImage={`url(${tshirt} )`} />
-			<Destaques productImage={`url(${tshirt} )`} />
-		</div>
+		<>
+			<div className={styles.title}>
+				<h3>Coleções em destaque</h3>
+			</div>
+			<div className={styles.wrapper}>
+				<div className={styles.cards}>
+					<Destaques productImage={`url(${tshirt} )`} />
+					<Destaques productImage={`url(${tshirt} )`} />
+					<Destaques productImage={`url(${tshirt} )`} />
+				</div>
+				<div className={styles.subTitle}>
+					<h3>Coleções em destaque</h3>
+				</div>
+
+				<div className={styles.icones}>
+					<div className={styles.iconeItem}>
+						<img src={camiseta} alt="camiseta" />
+						<p>Camisetas</p>
+					</div>
+					<div className={styles.iconeItem}>
+						<img
+							src={calça}
+							alt="calça"
+							style={{ width: "50px", height: "64px" }}
+						/>
+						<p>Calças</p>
+					</div>
+					<div className={styles.iconeItem}>
+						<img src={fone} alt="fone" />
+						<p>Headphones</p>
+					</div>
+					<div className={styles.iconeItem}>
+						<img src={tenis} alt="tenis" />
+						<p>Tenis</p>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 }
--- src/components/Destaques/index.jsx
+++ src/components/Destaques/index.jsx
@@ -2,21 +2,29 @@ import Botao from "../Botao";
 import DestaqueDesconto from "../DestaqueDesconto";
 
 import styles from "./styles.module.scss";
+import tshirt from "../assets/t-shit.png";
 
 export default function CardDestaques(props) {
 	return (
-		<div
-			className={styles.CardContainer}
-			style={{
-				backgroundImage: props.productImage,
-				backgroundRepeat: "no-repeat",
-				backgroundPosition: "right bottom",
-			}}
-		>
-			<div>
-				<DestaqueDesconto texto={"30% OFF"} />
-				<h3 className={styles.cardTitle}>Novo drop Supreme</h3>
-				<Botao texto={"Compra"} cor={"#f5f5f5"} largura={"114px"} />
+		<div className="wrapper">
+			<div
+				className={styles.CardContainer}
+				style={{
+					backgroundImage: `url(${tshirt} )`,
+					backgroundRepeat: "no-repeat",
+					backgroundPosition: "right bottom",
+				}}
+			>
+				<div className={styles.leftSide}>
+					<DestaqueDesconto texto={"30% OFF"} />
+					<h3 className={styles.cardTitle}>Novo drop Supreme</h3>
+					<Botao
+						texto={"Compra"}
+						corFundo={"#f5f5f5"}
+						cor={"#c92071"}
+						largura={"114px"}
+					/>
+				</div>
 			</div>
 		</div>
 	);
