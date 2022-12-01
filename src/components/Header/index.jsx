f865ae4eccab79e671357ef56227bb6345c3619b
Emmanuel <emmanuelfgadelha@gmail.com>
2022-12-01 00:14:25 -0300

    aplicado a responsividade e adcionado o icon carrinho

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,36 +4,53 @@ import { CiSearch } from 'react-icons/ci'
 import { IconContext } from "react-icons";
 import NavLink from "../NavLink";
 import Botao from "../Botao";
+import Carrinho from "../Carrinho"
 
 export default function Header() {
 
-    return (<div>
-        {/* <div style={location.pathname==="/home" && {color:"red"}}>
-            home
-        </div> */}
-        <div style={{ width: "1440px", height: "192px", left: "0px", top: "0px", display: "flex" }}>
-            <div className={styles.imgContainer}>
-                <img src={logo} alt="" />
-            </div>
-            <div className={styles.logo}>
-                Digital Store
-            </div>
-            <div className={styles.search}>
-                <input type="text" className={styles.inputBuscaHome} placeholder="Pesquisar produto..." />
-            </div>
-            <IconContext.Provider value={{ color: "rgb (102,102,102)", className: styles.icon }}>
-                <div>
-                    <CiSearch />
-                </div>
-            </IconContext.Provider>
-            <div className={styles.divBotaoEntrar}>
-                <Botao corFundo="#C92071" texto="Entrar" largura="114px" />
-            </div>
-            <div className={styles.divCadastro}>
-                Cadastre-se
-            </div>
-                <NavLink/>
-        </div>
-    </div>)
+        return (
+
+                <div className={styles.centralizar}>
+
+
+                        <div className={styles.containerHeader}>
+
+                                <div className={styles.topoHeader}>
+                                        <div className={styles.imgContainer}>
+                                                <img src={logo} alt="" className={styles.imagemLogo} />
+                                                <div className={styles.tituloLogo}>
+                                                        Digital Store
+                                                </div>
+                                        </div>
+
+                                        <div className={styles.inputContainer}>
+                                                <input type="text" className={styles.inputBuscaHome} placeholder="Pesquisar produto..." />
+                                                <IconContext.Provider value={{ color: "rgb (102,102,102)", className: styles.icon }}>
+                                                        <div>
+                                                                <CiSearch />
+                                                        </div>
+                                                </IconContext.Provider>
+                                        </div>
+                                        <div className={styles.botoesContainer}>
+
+                                                <div className={styles.divCadastro}>
+                                                        Cadastre-se
+                                                </div>
+
+                                                <div className={styles.divBotaoEntrar}>
+                                                        <Botao corFundo="#C92071" texto="Entrar" largura="7.87vw" />
+                                                </div>
+
+                                                <div className={styles.Carrinho}>
+                                                        <Carrinho />
+                                                </div>
+
+                                        </div>
+                                </div>
+                                <div className={styles.divNavLink}>
+                                        <NavLink />
+                                </div>
+                        </div>
+                </div>)
 }
 
