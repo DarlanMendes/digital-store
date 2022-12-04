0a4630cb4e5a6850b88803b3f8b228d4ca39e9c7
Antonio <tonny858895>
2022-12-04 12:48:40 -0300

    Correções Iniciais

--- src/components/CadastroContainer/index.jsx
+++ src/components/CadastroContainer/index.jsx
@@ -3,8 +3,16 @@ import faceLogo from '../../assets/images/face-logo.png'
 import Botao from '../Botao'
 export default function CadastroContainer() {
     return (<div className={styles.containerCadastro}>
-        <h1>Antonio</h1>
-        <img src={faceLogo} alt ="logo facebook"/>
-        <Botao cor={'red'} corFundo={'blue'} largura={'200px'} texto={'entre aqui'}/>
+        <div className='subcontainerPrincipal'>
+            <h1>Crie sua conta</h1>
+            <p className='conviteAoCadastro'>Já possui uma conta? Entre <u>aqui</u>.</p>
+            <p className='emailComAsterisco'>Email *</p>
+            <input type="text" className='campoDeDigitacaoDoEmail' placeholder='Insira seu email' />
+            <Botao cor={'white'} corFundo={'#C92071'} largura={'255px'} altura={'48px'} texto={'Criar Conta'} />
+        </div>
+        <div className='subcontainerSecundario'>
+            {/* <img src={faceLogo} alt="logo facebook" /> */}
+        </div>
+
     </div>)
-}
+}                                                                                                          
