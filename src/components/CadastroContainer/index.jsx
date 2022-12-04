b1ab08152c2209779ee98e10080cdd102adc9cb5
Antonio <tonny858895>
2022-12-03 22:08:11 -0300

    Pontapé Inicial

--- src/components/CadastroContainer/index.jsx
+++ src/components/CadastroContainer/index.jsx
@@ -3,8 +3,18 @@ import faceLogo from '../../assets/images/face-logo.png'
 import Botao from '../Botao'
 export default function CadastroContainer() {
     return (<div className={styles.containerCadastro}>
-        <h1>Antonio</h1>
-        <img src={faceLogo} alt ="logo facebook"/>
-        <Botao cor={'red'} corFundo={'blue'} largura={'200px'} texto={'entre aqui'}/>
+        <div className='subcontainerPrincipal'>
+            <h1>Acesse sua conta</h1>
+            <p className='conviteAoCadastro'>Novo cliente? Então registre-se <u>aqui</u>.</p>        
+            <p className='textoDoLogin'>Login *</p>
+            <span className='retanguloDoLogin'>
+                <input type="text" placeholder='Insira seu login ou email' />
+            </span>
+        </div>
+        <div className='subcontainerSecundario'>
+            <img src={faceLogo} alt="logo facebook" />
+            <Botao cor={'red'} corFundo={'blue'} largura={'200px'} texto={'entre aqui'} />
+        </div>
+
     </div>)
 }
