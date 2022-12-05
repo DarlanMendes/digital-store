49ff9b32b7c0e2b76b0ce4e954cb8265e2914629
Antonio <tonny858895>
2022-12-05 20:58:01 -0300

    Pequenas Inserções e Correções

--- src/components/CadastroContainer/index.jsx
+++ src/components/CadastroContainer/index.jsx
@@ -5,14 +5,14 @@ export default function CadastroContainer() {
     return (<div className={styles.containerCadastro}>
         <div className='subcontainerPrincipal'>
             <h1>Crie sua conta</h1>
-            <p className='conviteAoCadastro'>Já possui uma conta? Entre <u>aqui</u>.</p>
+            <p className='conviteAoCadastramento1'>Já possui uma conta? Entre <u>aqui</u>.</p>
             <p className='emailComAsterisco'>Email *</p>
             <input type="text" className='campoDeDigitacaoDoEmail' placeholder='Insira seu email' />
             <Botao cor={'white'} corFundo={'#C92071'} largura={'255px'} altura={'48px'} texto={'Criar Conta'} />
         </div>
         <div className='subcontainerSecundario'>
+            <p className='conviteAoCadastramento2'>Ou faça login com</p> 
             {/* <img src={faceLogo} alt="logo facebook" /> */}
-        </div>
-
-    </div>)
-}                                                                                                          
+        </div>  
+    </div>) 
+}                                                                                                           
