fdff3a30565aaff866f7bca0b52bcb6fc28f6bb9
Antonio <tonny858895>
2022-12-06 02:33:09 -0300

    Correções nas Classes dos Elementos

--- src/components/CadastroContainer/index.jsx
+++ src/components/CadastroContainer/index.jsx
@@ -3,16 +3,16 @@ import faceLogo from '../../assets/images/face-logo.png'
 import Botao from '../Botao'
 export default function CadastroContainer() {
     return (<div className={styles.containerCadastro}>
-        <div className='subcontainerPrincipal'>
+        <div className={styles.subcontainerPrincipal}>
             <h1>Crie sua conta</h1>
-            <p className='conviteAoCadastramento1'>Já possui uma conta? Entre <u>aqui</u>.</p>
-            <p className='emailComAsterisco'>Email *</p>
-            <input type="text" className='campoDeDigitacaoDoEmail' placeholder='Insira seu email' />
+            <p className={styles.conviteAoCadastramento1}>Já possui uma conta? Entre <u>aqui</u>.</p>
+            <p className={styles.emailComAsterisco}>Email *</p>
+            <input type="text" className={styles.campoDeDigitacaoDoEmail} placeholder='Insira seu email' />
             <Botao cor={'white'} corFundo={'#C92071'} largura={'255px'} altura={'48px'} texto={'Criar Conta'} />
         </div>
-        <div className='subcontainerSecundario'>
-            <p className='conviteAoCadastramento2'>Ou faça login com</p> 
+        <div className={styles.subcontainerSecundario}>
+            <p className={styles.conviteAoCadastramento2}>Ou faça login com</p> 
             {/* <img src={faceLogo} alt="logo facebook" /> */}
         </div>  
     </div>) 
-}                                                                                                           
+}                                                                               
