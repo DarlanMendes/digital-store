b80d8d788f160c4a9863919bce5262e08d194ba8
Emerson Souza <souzaemerson003@gmail.com>
2022-12-08 23:53:33 -0300

    mudancas no componente botao

--- src/components/Botao/index.jsx
+++ src/components/Botao/index.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styles from './styles.module.scss';
 export default function Botao(props){
     return(
@@ -7,20 +6,3 @@ export default function Botao(props){
         </button>
     )
 }
-=======
-import styles from "./styles.module.scss";
-export default function Botao(props) {
-	return (
-		<button
-			className={styles.botao}
-			style={{
-				backgroundColor: props.corFundo,
-				width: props.largura,
-				color: props.cor,
-			}}
-		>
-			{props.texto}
-		</button>
-	);
-}
->>>>>>> 2dc9653090fc8667a01f5bb16f1cdc324d8e71d2
