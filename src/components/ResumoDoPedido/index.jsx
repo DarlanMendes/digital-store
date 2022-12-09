7260950abbe0bac1cce38d1461c72c6c7aa5f4c7
Emerson Souza <souzaemerson003@gmail.com>
2022-12-08 23:51:59 -0300

    preparando a aba de meus pedidos com os devidos componentes, otimizacoes de alguns componentes na página meus pedidos

--- src/components/ResumoDoPedido/index.jsx
+++ src/components/ResumoDoPedido/index.jsx
@@ -1,30 +1,47 @@
 import estilo from './styles.module.scss'
-import Botao from '../../components/Botao'
+import Botao from '../Botao'
 
 export default function MeusPedidos() {
     return (<div>
-        <div className={estilo.container}>
-            <div className={estilo.containerValores}>
-                <label htmlFor="">RESUMO</label>
-                <hr />
-                <div className={estilo.subtotal}>
-                    <label htmlFor="">Subtotal</label>
-                    <div>R$ 219,00</div>
+        <div className={estilo.containerGeral}>
+
+            <div className={estilo.container}>
+                <div className={estilo.containerValores}>
+                    <label htmlFor="">RESUMO</label>
+                    <hr />
+                    <div className={estilo.subtotal}>
+                        <label htmlFor="">Subtotal</label>
+                        <div>R$ 219,00</div>
+                    </div>
+                    <div className={estilo.frete}>
+                        <label htmlFor="">Frete</label>
+                        <div>R$ 0,00</div>
+                    </div>
+                    <div className={estilo.desconto}>
+                        <label htmlFor="">Desconto</label>
+                        <div>R$ 30,00</div>
+                    </div>
+                    <div className={estilo.total}>
+                        <label htmlFor="">Total</label>
+                        <div>R$ 219,00</div>
+                    </div>
+                    <div className={estilo.parcelamento}> ou 10x de R$ 21,00 sem juros</div>
+                    <div className={estilo.botao}>
+                        <Botao cor={'#F5F5F5'} corFundo={'#F6AA1C'} largura={'274px'} altura={'40px'} texto={'Continuar'} />
+                    </div>
                 </div>
-                <div className={estilo.frete}>
-                    <label htmlFor="">Frete</label>
-                    <div>R$ 0,00</div>
+            </div>
+            <div className={estilo.containerTotal}>
+                <div className={estilo.totalComBotao}>
+                    <div className={estilo.total}>
+                        <label htmlFor="">Total</label>
+                        <div>R$ 219,00</div>
+                    </div>
+                    <div className={estilo.parcelamento}> ou 10x de R$ 21,00 sem juros</div>
+                    <div className={estilo.botao}>
+                        <Botao cor={'#F5F5F5'} corFundo={'#F6AA1C'} largura={'315px'} altura={'40px'} texto={'Continuar'} />
+                    </div>
                 </div>
-                <div className={estilo.desconto}>
-                    <label htmlFor="">Desconto</label>
-                    <div>R$ 30,00</div>
-                </div>
-                <div className={estilo.total}>
-                    <label htmlFor="">Total</label>
-                    <div>R$ 219,00</div>
-                </div>
-                <div className={estilo.parcelamento}> ou 10x de R$ 21,00 sem juros</div>
-                <Botao className={estilo.botao} cor={'#F5F5F5'} corFundo={'#F6AA1C'} largura={'274px'} altura={'40px'}texto={'Continuar'}/>
             </div>
         </div>
     </div>)
--- src/pages/MeusPedidos/index.jsx
+++ src/pages/MeusPedidos/index.jsx
@@ -1,105 +1,30 @@
 import estilo from './styles.module.scss'
-import tenis from './assets/White-Sneakers-PNG-Clipart 2.png'
-import Botao from '../../components/Botao'
+import Resumo from '../../components/ResumoDoPedido'
+import Carrinho from '../../components/MeuCarrinho'
+import Produto from '../../components/ProdutoCard'
 
-export default function MeusPedidos() {
-    return (<div>
+export default function ProdutoHome() {
+    return (
         <div className={estilo.container}>
-            <div className={estilo.carrinho}>
-                <div className={estilo.nomes}>
-                    <div className={estilo.titulo}>
-                        MEU CARRINHO
-                    </div>
-                    <div className={estilo.subtitulos}>
-                        <div className={estilo.subQuantidade}>
-                            QUANTIDADE
-                        </div>
-                        <div className={estilo.subUnitário}>
-                            UNITÁRIO
-                        </div>
-                        <div className={estilo.subTotal}>
-                            TOTAL
-                        </div>
-
-                    </div>
+            <div className={estilo.primeiraLinha}> 
+                <div>
+                    <Carrinho />
                 </div>
-                <hr />
-                <div className={estilo.fileirasProdutos}>
-                    <div className={estilo.idProduto}>
-                        <div className={estilo.imagemProduto}>
-                            <img src={tenis} alt="" />
-                        </div>
-                        <div className={estilo.especificacoesProduto}>
-                            <div className={estilo.nomeProduto}>
-                                Tênis Nike Revolution 6 Next Nature Masculino
-                            </div>
-                            <div className={estilo.corProduto}>
-                                <label>Cor:</label> Vermelho / Branco
-                            </div>
-                            <div className={estilo.especProduto}>
-                                <label>Tamanho:</label> 42
-                            </div>
-                        </div>
-                    </div>
-                    <div className={estilo.informacaoPedidos}>
-                        <div className={estilo.quantidade}>
-                            <div className={estilo.botoes}>
-                                <button> - </button>
-                                <p>1</p>
-                                <button> + </button>
-                            </div>
-                            <p className={estilo.removerItem}>Remover item</p>
-                        </div>
-                        <div className={estilo.unidades}>
-                            <div className={estilo.normalPrice}>
-                                R$ 219,00
-                            </div>
-                            <div className={estilo.discountPrice}>
-                                R$ 219,00
-                            </div>
-                        </div>
-                        <div className={estilo.total}>
-                            <div className={estilo.normalPrice}>
-                                R$ 219,00
-                            </div>
-                            <div className={estilo.discountPrice}>
-                                R$ 219,00
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <hr />
-                <div className={estilo.informacoesAdicionais}>
-                    <div className={estilo.desconto}>
-                        <label htmlFor="">Cupom de desconto</label>
-                        <div>
-                            <input type="text" name="" id="" placeholder="Insira seu codigo" /> <Botao cor={'#C92071'} corFundo={'#F5F5F5'} largura={'114px'} altura={'60px'} texto={'OK'} />
-                        </div>
-                    </div>
-                    <div className={estilo.containerDescontoMobile}>
-                        <div className={estilo.descontoMobile}>
-                            <label htmlFor="">Cupom de desconto</label>
-                            <div>
-                                <input type="text" name="" id="" placeholder="Insira seu codigo" /> <Botao cor={'#C92071'} corFundo={'#F5F5F5'} largura={'255px'} altura={'60px'} texto={'OK'} />
-                            </div>
-                        </div>
-                    </div>
-                    <div className={estilo.frete}>
-                        <label htmlFor="">Calcular frete</label>
-                        <div>
-                            <input type="text" name="" id="" placeholder="Insira seu CEP" /> <Botao cor={'#C92071'} corFundo={'#F5F5F5'} largura={'114px'} altura={'60px'} texto={'OK'} />
-                        </div>
-                    </div>
-                    <div className={estilo.containerFreteMobile}>
-                        <div className={estilo.freteMobile}>
-                        <label htmlFor="">Calcular frete</label>
-                        <div>
-                            <input type="text" name="" id="" placeholder="Insira seu CEP" /> <Botao cor={'#C92071'} corFundo={'#F5F5F5'} largura={'255px'} altura={'60px'} texto={'OK'} />
-                        </div>
-                        </div>
-                    </div>
+                <div>
+                    <Resumo />
                 </div>
             </div>
+            <div className={estilo.segundaLinha}>
+                <div className={estilo.titulo}>
+                    Produtos Relacionados
+                </div>
+                <a href="">Ver todos -{">"}</a>
+            </div>
+            <div className={estilo.terceiraLinha}>
+                <Produto />
+                <Produto />
+                <Produto />
+            </div>
         </div>
-    </div>)
+    )
 }
