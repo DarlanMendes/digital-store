f712f8acb2615769c3077a75d77db9f681fc0667
Darlan Mendes <dedsondarlan@gmail.com>
2022-12-18 11:40:12 -0300

    Adição de link no produto para levar a tela de detalhes do produto

--- src/components/ProdutoCard/index.jsx
+++ src/components/ProdutoCard/index.jsx
@@ -1,23 +1,28 @@
 import tenis from './Images/tenis.png'
 import estilo from './styles.module.scss'
-
-export default function ProdutoHome() {
+import { Link } from 'react-router-dom'
+export default function ProdutoHome(props) {
     return (
+
         <div className={estilo.ProdutoGeral}>
-            <div className={estilo.Produto}>
-                <div className={estilo.caixaImagem}>
-                    <img src={tenis} alt="" className={estilo.ImagemProduto}/>
+            <Link to={"/produtos/" + props.id}>
+                <div className={estilo.Produto}>
+                    <div className={estilo.caixaImagem}>
+                        <img src={tenis} alt="" className={estilo.ImagemProduto} />
+                    </div>
+                    <div className={estilo.TipoProduto}>
+                        Tenis
+                    </div>
+                    <div className={estilo.NomeProduto}>
+                        K-Swiss V8 - Masculino
+                    </div>
+                    <div className={estilo.PrecoProduto}>
+                        $100
+                    </div>
                 </div>
-                <div className={estilo.TipoProduto}>
-                    Tenis
-                </div>
-                <div className={estilo.NomeProduto}>
-                    K-Swiss V8 - Masculino
-                </div>
-                <div className={estilo.PrecoProduto}>
-                    $100
-                </div>
-            </div>
+            </Link>
         </div>
+
+
     )
 }
