e7bba33411ae297d4b77dd5253e08f951770560b
Emerson Souza <souzaemerson003@gmail.com>
2022-12-27 16:09:02 -0300

    adicionando todos os componentes da tela de finalizar compra

---
+++ src/components/FinalizaCompra/index.jsx
@@ -0,0 +1,31 @@
+import estilo from './styles.module.scss'
+import Botao from '../Botao'
+
+export default function FinalizarCompra() {
+    return (
+        <div className={estilo.container}>
+            <div className={estilo.titulo}>
+                Finalizar Compra
+            </div>
+            <hr />
+            <div className={estilo.total}>
+                <div className={estilo.tituloTotal}>
+                    Total
+                </div>
+                <div className={estilo.divValor}>
+                    <div className={estilo.valor}>
+                        R$ 219,00
+                    </div>
+                    <div className={estilo.parcelamento}>
+                        ou 10x de R$ 21,00 sem juros
+                    </div>
+                </div>
+            </div>
+            <div className={estilo.botao}>
+                <Botao cor={'white'} corFundo={'#F6AA1C'} largura={'100%'} altura={'50px'} texto={'Realizar Pagamento'} />
+            </div>
+        </div>
+
+    )
+}
+
---
+++ src/components/FinalizarCompraResumo/index.jsx
@@ -0,0 +1,59 @@
+import estilo from './styles.module.scss'
+import img from './assets/White-Sneakers-PNG-Clipart 2.svg'
+import Botao from '../../components/Botao'
+export default function InformacoesPagamento() {
+
+    return (
+        <div className={estilo.container}>
+            <div className={estilo.titulo}>
+                RESUMO
+            </div>
+            <hr />
+            <div className={estilo.produtos}>
+                <div className={estilo.imgProduto}>
+                    <img src={img} />
+                </div>
+                <div className={estilo.nomeProduto}>
+                    Tênis Nike Revolution 6 Next Nature Masculino
+                </div>
+            </div>
+            <hr />
+            <div className={estilo.subtotal}>
+                <label>Subtotal:</label>
+                <div>
+                    R$ 219,00
+                </div>
+            </div>
+            <div className={estilo.frete}>
+                <label>Frete:</label>
+                <div>
+                    R$ 0,00
+                </div>
+            </div>
+            <div className={estilo.desconto}>
+                <label>Desconto:</label>
+                <div>
+                    R$ 30,00
+                </div>
+            </div>
+            <div className={estilo.total}>
+                <div className={estilo.divValor}>
+                    <div className={estilo.tituloTotal}>
+                        Total
+                    </div>
+                    <div className={estilo.valor}>
+                        R$ 219,00
+                    </div>
+                </div>
+                <div className={estilo.parcelamento}>
+                    ou 10x de R$ 21,00 sem juros
+                </div>
+            </div>
+            <div>
+                <div className={estilo.botao}>
+                    <Botao cor={'white'} corFundo={'#F6AA1C'} largura={'100%'} altura={'50px'} texto={'Realizar Pagamento'} />
+                </div>
+            </div>
+        </div>)
+
+}
