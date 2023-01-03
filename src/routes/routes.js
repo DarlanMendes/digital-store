cf9944fef52323843110e9856de7d6a91ae179cd
Darlan Mendes <dedsondarlan@gmail.com>
2023-01-03 17:19:14 -0300

    Correção de rotas do navlink

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,4 +2,4 @@ export const route = [{ title: 'Home', route: '/home' },
 { title: 'Produtos', route: '/produtos' },
 { title: 'Categorias', route: '/categorias' },
 { title: 'Meus Pedidos', route: '/meuspedidos' },
-{ title: 'Finalizar Compra', route: '/finalizarcompra'}]
+]
