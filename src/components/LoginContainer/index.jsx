cee7c830cc4e6996b80b9d6416dcabf053fac04b
Davi Roque Maia de Sousa <sowdavi@gmail.com>
2023-01-06 23:31:46 -0300

    responsive login container

--- src/components/LoginContainer/index.jsx
+++ src/components/LoginContainer/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './styles.module.scss'
 import faceLogo from '../../assets/images/face-logo.png'
 import Botao from '../Botao'
@@ -9,6 +9,26 @@ import gmailLogo from '../../assets/images/gmail-logo.png'
 export default function CadastroContainer() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [widthButton, setWidthButton] = useState('')
+
+    useEffect(() => {
+        checkSize()
+    })
+
+
+
+    window.addEventListener("resize", () => {
+        checkSize()
+    })
+
+    const checkSize = () => {
+        if (window.innerWidth < 500) {
+            setWidthButton('255px');
+        } else {
+            setWidthButton('523px');
+        }
+    }
+
 
     const handleChange = (e) => {
         setEmail(e.target.value);
@@ -32,22 +52,26 @@ export default function CadastroContainer() {
 
 
     return (<div className={styles.containerLogin}>
-      
-            <h1 className={styles.textMain}>Acesse sua conta</h1>
-            <h3 className={styles.textSubMain}>Novo cliente? Então registre-se <span className={styles.underscore}>aqui.</span></h3>
-            <form onSubmit={handleSubmit}>
-                <h3 className={styles.indicativeText}>Login *</h3>
-                <input type="email" value={email} className={styles.inputLogin} required onChange={(e) => { handleChange(e) }} placeholder='Insira seu login ou email' />  
-                <h3 className={styles.indicativeText}>Senha *</h3>
-                <input type="password" value={password} className={styles.inputLogin} required onChange={(e) => { handlePasswordChange(e) }} placeholder='Insira sua senha' />
-                <h3 className={styles.helpPassword}>Esqueci minha senha</h3>
-                <Botao cor={'white'} corFundo={'#C92071'} largura={'523px'} texto={'Acessar conta'} altura={'48px'} />
-            </form>
-            <div className={styles.containerLoginAlternative}>
-            <h3 className={styles.textLoginAlternative}>Ou faça login com</h3>
-            <img src={gmailLogo} className={styles.logoImg} alt="" />
-            <img src={faceLogo} alt="" />
+
+        <h1 className={styles.textMain}>Acesse sua conta</h1>
+        <h3 className={styles.textSubMain}>Novo cliente? Então registre-se <span className={styles.underscore}>aqui.</span></h3>
+        <form onSubmit={handleSubmit}>
+            <h3 className={styles.indicativeText}>Login *</h3>
+            <input type="email" value={email} className={styles.inputLogin} required onChange={(e) => { handleChange(e) }} placeholder='Insira seu login ou email' />
+            <h3 className={styles.indicativeText}>Senha *</h3>
+            <input type="password" value={password} className={styles.inputLogin} required onChange={(e) => { handlePasswordChange(e) }} placeholder='Insira sua senha' />
+            <h3 className={styles.helpPassword}>Esqueci minha senha</h3>
+            <Botao cor={'white'} corFundo={'#C92071'} largura={widthButton} texto={'Acessar conta'} altura={'48px'} />
+        </form>
+        <div className={styles.containerLoginAlternative}>
+            <div className={styles.divAlternative} >
+                <h3 className={styles.textLoginAlternative}>Ou faça login com</h3>
             </div>
-       
+            <div className={styles.divLogoAlternative} >
+                <img src={gmailLogo} className={styles.logoImg} alt="" />
+                <img src={faceLogo} alt="" />
+            </div>
+        </div>
+
     </div>)
 }
